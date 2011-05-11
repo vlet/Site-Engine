8ba62d7da41ca6ed0bae5ab7b1602f1bd3deaff0
Vladimir Lettiev <thecrux@gmail.com>
2011-05-11 12:01:30 +0400

    Update engine
     + templates FOREACH/END, SET, INCLUDE, IF/FI
     + eval route execution ( 500 page on error with debug )

--- lib/Site/Engine.pm
+++ lib/Site/Engine.pm
@@ -26,6 +26,54 @@ sub _escape_html ($) {
     return $data;
 }
 
+sub _if ($$$) {
+    my ($var, $block, $vars) = @_;
+    return $block if ( exists $vars->{$var} && $vars->{$var} );
+    "";
+}
+
+sub _set ($$$) {
+    my ($var, $value, $vars) = @_;
+    $vars->{$var} = $value;
+    "";
+}
+
+sub _include ($$) {
+    my ($template, $vars) = @_;
+    &_template($template, $vars);
+}
+
+sub _vars ($$) {
+    my ($ref, $vars) = @_;
+    $$ref =~ s/\[%\sSET\s(\w+)\s?=\s?\"(.+?)\"\s%\]/_set $1,$2,$vars/eg;
+    $$ref =~ s/\[%\sINCLUDE\s(\w+)\s%\]/_include $1, $vars/eg;
+    $$ref =~ s/\[%\sIF\s([\w\.]+)\s%\](.+?)\[%\sFI\s%\]/_if $1,$2,$vars/seg;
+    $$ref =~ s/\[%\s([\w\.]+?)\s\|\sraw\s%\]/(exists $vars->{$1} )?$vars->{$1}:""/eg;
+    $$ref =~ s/\[%\s([\w\.]+?)\s%\]/_escape_html $vars->{$1}/eg;
+
+    # remove all other tags
+    $$ref =~ s/\[%.+?%\]//g;
+}
+
+sub _foreach ($$$$) {
+    my ($hash_name, $array_name, $block, $vars) = @_;
+    my $output = "";
+    return "" if (! exists $vars->{$array_name});
+    foreach my $h_ref ( @{ $vars->{$array_name} } ) {
+        foreach my $key ( keys %$h_ref ) {
+            $vars->{$hash_name.".".$key} = $h_ref->{$key};
+        }
+        my $copy = $block;
+        _vars \$copy, $vars;
+        $output .= $copy;
+        foreach my $key ( keys %$h_ref ) {
+            delete $vars->{$hash_name.".".$key};
+        }
+    }
+    return $output;
+}
+
+
 sub _template ($$) {
     my ($template, $vars) = @_;
     my $file = $config->{htdocs} . "/" . $template . ".tt";
@@ -34,8 +82,8 @@ sub _template ($$) {
     open my $fh, "<", $file or die $!;
     my $data = <$fh>;
     close $fh;
-    $data =~ s/\[:\s(\w+?)\s\|\sraw\s:\]/(exists $vars->{$1} )?$vars->{$1}:""/eg;
-    $data =~ s/\[:\s(\w+?)\s:\]/_escape_html $vars->{$1}/eg;
+    $data =~ s/\[%\sFOREACH\s(\w+)\sIN\s(\w+)\s%\](.+?)\[%\sEND\s%\]/_foreach $1,$2,$3,$vars/seg;
+    _vars \$data, $vars;
     return $data;
 }
 
@@ -85,20 +133,41 @@ sub start_site ($) {
     foreach my $route (@routes) {
         if ($request_method eq $route->[0] && $path_info =~ /^$route->[1]$/ ) {
             my @matches = ($path_info =~ /^$route->[1]$/);
-            $body = $route->[2]->(@matches);
+            eval {
+                $body = $route->[2]->(@matches);
+            };
+            if ($@) {
+                my $err = "";
+                if ($config->{debug}) {
+                    $err = _escape_html($@);
+                } 
+                $body = qq{
+                    <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+                    <html><head>
+                    <title>500 Internal Error</title>
+                    </head><body>
+                    <h1>Sorry, error</h1>
+                    <pre>$err</pre>
+                    <p>Please report to webmaster.</p>
+                    </body></html>
+                };
+                $body =~ s/\n\s+/\n/sg;
+                header "Status" => 500;
+            }
             last;
         }
     }
     if (!$body) {
-        $body =<<EOF;
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<html><head>
-<title>404 Not Found</title>
-</head><body>
-<h1>Not Found</h1>
-<p>The requested URL was not found on this server.</p>
-</body></html>
-EOF
+        $body = qq{
+            <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+            <html><head>
+            <title>404 Not Found</title>
+            </head><body>
+            <h1>Not Found</h1>
+            <p>The requested URL was not found on this server.</p>
+            </body></html>
+        };
+        $body =~ s/\n\s+/\n/sg;
         header "Status" => 404;
     }
 
