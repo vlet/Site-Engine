08a0ac07ca87b6d071d94d732ca0e3e2a7976e39
Vladimir Lettiev <thecrux@gmail.com>
2011-05-21 21:44:27 +0400

    Method dbh (instead of connect). Support for sqlite and mysql.

--- lib/Site/Engine/Database.pm
+++ lib/Site/Engine/Database.pm
@@ -1,16 +1,37 @@
 package Site::Engine::Database;
 use strict;
 use warnings;
+use DBI;
 use Exporter qw( import );
-our @EXPORT = qw( connect );
+our @EXPORT = qw( dbh );
 our $VERSION = '0.01';
 
 my $config;
+my $dbh;
+my $dbi = {
+    sqlite => sub {
+        my $db_conf = shift;
+        DBI->connect("dbi:SQLite:dbname=". $db_conf->{db_file}, "", "", { RaiseError => 1, sqlite_unicode => 1 });
+    },
+    mysql => sub {
+        my $db_conf = shift;
+        DBI->connect("DBI:mysql:database=". $db_conf->{db} .
+            ( (exists $db_conf->{db_host})?";host=" . $db_conf->{db_host}:"" ) .
+            ( (exists $db_conf->{db_port})?";port=" . $db_conf->{db_port}:"" )
+            , $db_conf->{db_user}, $db_conf->{db_pass}, { RaiseError => 1, mysql_enable_utf8 => 1});
+    },
+};
 
 # Public
 
+sub dbh {
+    $dbh;    
+}
+
 sub init ($) {
     $config = shift;
+    die "Unsupported type of db" if (!exists $dbi->{ $config->{db}->{type} } );
+    $dbh = $dbi->{ $config->{db}->{type} }->( $config->{db} );
 }
 
 1;
